cd211672c4dd4d8df410d0be0c64cd41881137f6
Fernanda Dias <fernanda.dias.silva@accenture.com>
2024-11-08 20:44:56 -0300

    chore add shadcn  setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
+import { Button } from "./_components/ui/button";
+
 const Home = () => {
-  return ( <h1 className="text-red-500 p-5">Hello World</h1> );
-}
- 
+  return <Button> Hello</Button>;
+};
+
 export default Home;
